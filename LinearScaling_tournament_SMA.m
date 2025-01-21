b1aaff8b72eafd50ee5a58534667deb1df726566
ImMohammadHosseini <sayedmh50@gmail.com>
2025-01-21 13:41:21 +0330

    add tournament and linear scaling to SMA

---
+++ LinearScaling_tournament_SMA.m
@@ -0,0 +1,89 @@
+
+function [Convergence_curve,Ave]=SMA(N,Max_iter,lb,ub,dim,fobj,Run_no)
+
+for irun=1:Run_no
+
+    bestPositions=zeros(1,dim);
+    Destination_fitness=inf;
+    AllFitness = inf*ones(N,1);
+    weight = ones(N,dim);
+
+    X=initialization(N,dim,ub,lb);
+    Convergence_curve=zeros(1,Max_iter);
+    it=1;  
+    lb=ones(1,dim).*lb; 
+    ub=ones(1,dim).*ub;
+    z=0.03; 
+
+
+    while  it <= Max_iter
+
+        for i=1:N
+            Flag4ub=X(i,:)>ub;
+            Flag4lb=X(i,:)<lb;
+            X(i,:)=(X(i,:).*(~(Flag4ub+Flag4lb)))+ub.*Flag4ub+lb.*Flag4lb;
+            AllFitness(i) = fobj(X(i,:));
+        end
+	minFitness = min(AllFitness);
+	maxFitness = max(AllFitness);
+	scaledFitness = (AllFitness - minFitness) / (maxFitness - minFitness);
+	scaledFitness = max(0, min(1, scaledFitness));  % Ensure within [0, 1]
+	
+	selected = tournament_selection(scaledFitness, N, tournament_size);
+        [SmellOrder,SmellIndex] = sort(scaledFitness(selected));  %Eq.(2.6)
+        worstFitness = SmellOrder(N);
+        bestFitness = SmellOrder(1);
+
+        S=bestFitness-worstFitness+eps;  
+
+        for i=1:N
+            for j=1:dim
+                if i<=(N/2)  
+                    weight(SmellIndex(i),j) = 1+rand()*log10((bestFitness-SmellOrder(i))/(S)+1);
+                else
+                    weight(SmellIndex(i),j) = 1-rand()*log10((bestFitness-SmellOrder(i))/(S)+1);
+                end
+            end
+        end
+
+
+        if bestFitness < Destination_fitness
+            bestPositions=X(SmellIndex(1),:);
+            Destination_fitness = bestFitness;
+        end
+
+        a = atanh(-(it/Max_iter)+1);   
+        b = 1-it/Max_iter;
+
+        for i=1:N
+            if rand<z    
+                X(i,:) = (ub-lb)*rand+lb;
+            else
+                p =tanh(abs(AllFitness(i)-Destination_fitness));  
+                vb = unifrnd(-a,a,1,dim);  
+                vc = unifrnd(-b,b,1,dim);
+                for j=1:dim
+                    r = rand();
+                    A = randi([1,N]);  
+                    B = randi([1,N]);
+                    if r<p    
+                        X(i,j) = bestPositions(j)+ vb(j)*(weight(i,j)*X(A,j)-X(B,j));
+                    else
+                        X(i,j) = vc(j)*X(i,j);
+                    end
+                end
+            end
+        end
+        Convergence_curve(it)=Destination_fitness;
+        best_run(irun)=Destination_fitness;
+
+        it=it+1;
+    end
+end
+Ave = mean(best_run);
+fprintf("\nSMA = %d",Ave);
+writeDataToFile(sprintf('SMA = %d', mean(best_run)), fullfile('Results', 'Numerical_results.txt'));
+% save(fullfile('Results', 'SMA_Result.mat'), 'Convergence_curve');
+save(fullfile('Results', strrep(func2str(fobj), '@', ''), 'SMA_Result.mat'), 'Convergence_curve');
+
+end
